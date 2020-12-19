b33fc0bcd0c8d4937a2648f7433f3c3d935d2ad4
Jonathan MARTIN <j.martin@lonsdale.fr>
2020-12-19 18:19:22 +0100

    access

--- modal.js
+++ modal.js
@@ -16,15 +16,58 @@ function Modal(){
 		const box = modal.querySelector('.box');
 		if(!box.contains(e.target) && !btn_close.contains(e.target)) close();
 	}
-
+	
+	const trap = {
+		index: 0,
+		els: [],
+		isShifted: false,
+		init(){
+			trap.els = [];
+			modal.querySelectorAll('button,a,input').forEach(el => trap.els.push(el));
+		},
+		keyup(e){
+			e.key === 'Escape' && close();
+			if(e.key === 'Shift') {
+				trap.isShifted = false;
+			}
+		},
+		keydown(e){
+			if(e.key === 'Shift') {
+				trap.isShifted = true;
+			}
+        	if(e.key === 'Tab') {
+           		if(e.preventDefault) e.preventDefault();
+				else e.returnValue = false;
+				trap.isShifted ? trap.index -- : trap.index ++;
+				if(trap.index < 0) trap.index = trap.els.length-1;
+				if(trap.index >= trap.els.length) trap.index = 0;
+				trap.els[trap.index].focus();
+			}
+		},
+		add(){
+			btn_close.focus();
+			document.addEventListener('keydown', trap.keydown, false);
+			document.addEventListener('keyup', trap.keyup, false);
+		},
+		remove(){
+			document.removeEventListener('keydown', trap.keydown);
+			document.removeEventListener('keyup', trap.keyup);
+		}
+	}
+	
+	let activeEl;
+	
 	const close = e => {
 		window.removeEventListener(clicktouch, clickOut);
 		modal.classList.add('hide');
 		modal.addEventListener('animationend', () => modal.remove(), {once:true});
 		window.removeEventListener('scroll', disableScroll);
+		trap.remove();
+		if(activeEl.type === 'button') activeEl.focus();
 	}
 
 	const open = (msg, type) => {
+		activeEl = document.activeElement;
 		scrollTop = window.pageYOffset || window.scrollY;
 		window.addEventListener('scroll', disableScroll);
 		modal = document.createElement('div');
@@ -34,7 +77,7 @@ function Modal(){
 			<div class="content">${msg}</div>
 			<div class="action">
 				<button class="btn-ok">ok</button>
-				<button class="btn-cancel">cancel</button>
+				${type === 'confirm' ? `<button class="btn-cancel">cancel</button>` : ``}
 			</div>
 		</div>`;
 		modal.classList.add(type);
@@ -42,6 +85,8 @@ function Modal(){
 		btn_close = modal.querySelector('.btn-close');
 		btn_close.onclick = () => close();
 		window.addEventListener(clicktouch, clickOut, {capture:true});
+		trap.init();
+		trap.add();
 	}
 
 	this.alert = msg => {
@@ -60,4 +105,5 @@ function Modal(){
 		};
 	}
 }
+
 export default Modal;
